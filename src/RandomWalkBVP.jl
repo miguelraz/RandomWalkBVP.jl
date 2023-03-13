860143a29b433ba90a0a2e843a9f03893ea81676
Miguel Raz Guzmán Macedo <miguelraz@ciencias.unam.mx>
2023-03-13 00:43:38 -0600

    yolo

--- src/RandomWalkBVP.jl
+++ src/RandomWalkBVP.jl
@@ -1,5 +1,80 @@
 module RandomWalkBVP
 
-# Write your package code here.
+abstract type AbstractRandomWalkerBVP end
+mutable struct RandomEnsemble{T} <: AbstractRandomWalkerBVP
+    const f::Function
+    const bitmat::BitMatrix
+    const view_bitmat::BitMatrix
+    walkers::Matrix{T}
+    temp_walkers::Matrix{T}
+    sol::Matrix{T}
+end
+
+function RandomEnsemble{T}(xs::Matrix{T}, f::Function) where T
+    n, m = size(xs)
+    b = [xs[i, j] == first(xs) for i in 1:n, j in 1:m] |> BitArray
+    b = shrink_grid(b)
+    vb = @view b[b]
+    w = zeros(T, n, m)
+    tw = zeros(T, n, m)
+    s = zeros(T, n, m)
+    RandomWalk{T}(f, b, vb, w, tw, s)
+end
+
+walk!(re::RandomEnsemble{T}, i, j) where T = (i, j) .+ rand(((0, 1), (0, -1), (1, 0), (-1, 0)))
+valid(re::RandomEnsemble{T}, i, j) where T = re.bitmat[i, j]
+
+function trajectory!(re::RandomEnsemble{T} i, j) where T
+    @assert all(==(zero(eltype(T))), walkers)
+    re.tempwalkers .= zero(eltype(T))
+
+    # TODO -> keep top/bot/left/right maximums and extract that window
+    while valid(re, i, j)
+        i, j = walk!(re, i, j)
+        re.temp_walkers[i,j] += 1
+    end
+    scalar = f(i, j)
+
+    re.sol .+= re.temp_walkers .* scalar
+
+    @assert walkers != zeros(size(sol))
+    walkers .= zero(eltype(T))
+    @assert all(==(0), walkers)
+    return sol
+end
+
+function shrink_grid(grid::BitMatrix)
+    left = findfirst(any.(>(0), eachcol(grid)))
+    right = findlast(any.(>(0), eachcol(grid)))
+
+    top = findfirst(any.(>(0), eachrow(grid)))
+    bot = findlast(any.(>(0), eachrow(grid)))
+    # TODO - handle corner cases where ±1 might not be inside the array
+    @view grid[top-1:bot+1, left-1:right+1]
+end
+
+function solve(re::RandomEnsemble{T}, n::Int) where T
+    n, m = size(re.bitmat)
+    while all(<(n), view_bitmat)
+        for j in 1:m
+            for i in 1:n
+                # Walkers
+                # Evolucionar caminante hasta frontera 
+                    # agregar a matriz de walkers
+                    # guardar valor en frontera
+                    # mul! sol <- fend * num_walks
+                caminante2!(i, j, mat, sol, numero_de_caminantes, f)
+            end
+            all(>=(n), view_walkers) && break
+        end
+    end
+    return re
+end
+
+export RandomWalk
+export valid
+export walk!
+export trajectory!
+export shrink_grid
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,76 @@
 using RandomWalkBVP
 using Test
 
-@testset "RandomWalkBVP.jl" begin
-    # Write your tests here.
+#using Tullio
+#using Folds
+using Random
+#using HDF5
+
+mat = h5read("hdf5mat", "placa") |> BitArray;
+@testset "Construction and basic functions" begin
+
+    #@test valid(mat, 200, 200)
+
+    #minimat = [false false false; false true false; false false false;] |> BitArray;
+    minimat = [0 0 0; 0 1 0; 0 0 0];
+    f(y, x) = 1 / (y + 1)
+    RandomWalk(minimat, f) isa RandomWalk
+    Random.seed!(42)
+    sol = zeros(size(minimat))
+    walkers = zeros(size(minimat))
+    RandomWalk(minimat)
+    @test sum(walkers) == 0
+    @test sum(sol) == 0
+    @test sum(minimat) == 1
+    sol = trayectoria!(2,2, sol, minimat, walkers, f)
+    @test sum(walkers) == 0
+    @show sol
+    @test sum(sol) ≈ 1/3
+    @test sum(minimat) == 1
+    for _ in 1:100
+        @test walk!(1,1) ∈ ((2,1), (0, 1), (1, 2), (1, 0))
+    end
+    @test 0 == @allocated walk!(1,1)
 end
+
+
+@testet "Shrink grid" begin
+    shs = shrink_grid(mat);
+    @test size(shs) == (335, 389)
+    @test 2 == findfirst(any.(>(0), eachrow(shs)))
+    @test 334 == findlast(any.(>(0), eachrow(shs)))
+    @test 2 == findfirst(any.(>(0), eachcol(shs)))
+    @test 388 == findlast(any.(>(0), eachcol(shs)))
+    # TODO idempotencia shs == eng
+    @test shs == shrink_grid(shs)
+end
+
+# INPUT: 
+# mat - BitMatrix de puntos interiores
+function resuelve_Laplace2(mat, f, n)
+    mat = shrink_grid(mat)
+    n1, n2 = size(mat)
+    sol = zeros(Float64, (n1, n2))
+    walkers = zeros(Float64, (n1, n2))
+
+    # TODO - reuse a single pushed_vector buffer
+    # setup PushVector{(T,T)}() 
+    view_walkers = @view walkers[mat]
+
+    while all(<(n), view_walkers)
+        for j in 1:n2
+            for i in 1:n1
+                # Walkers
+                # Evolucionar caminante hasta frontera 
+                    # agregar a matriz de walkers
+                    # guardar valor en frontera
+                    # mul! sol <- fend * num_walks
+                caminante2!(i, j, mat, sol, numero_de_caminantes, f)
+            end
+            all(>=(n), view_walkers) && break
+        end
+    end
+    return sol, numero_de_caminantes
+end
+
+@time sol, num = resuelve_Laplace2(mat, f, 1);
