9bcdc847b3afca09e161a79068f3dfc05f49763f
miguelraz <miguelraz@ciencias.unam.mx>
2023-03-13 22:53:07 -0600

    sub 10s

--- src/RandomWalkBVP.jl
+++ src/RandomWalkBVP.jl
@@ -1,9 +1,11 @@
 module RandomWalkBVP
+using Tullio
+using LoopVectorization
 
 __precompile__(false)
 abstract type AbstractRandomWalkerBVP end
-mutable struct RandomEnsemble{T} <: AbstractRandomWalkerBVP
-    const f::Function
+mutable struct RandomEnsemble{T,F} <: AbstractRandomWalkerBVP
+    const f::F
     # TODO - make these subarrays
     const bitmat::BitMatrix
     #const view_bitmat::AbstractArray{S, 2}
@@ -15,13 +17,11 @@ end
 function shrink_grid(grid)
     right = findlast(any.(>(0), eachcol(grid)))
     bot = findlast(any.(>(0), eachrow(grid)))
-    any(<(3), (right, bot))::Bool && error("Grid too small")
+    (right < 3) || (bot < 3) && error("Grid too small")
 
     # TODO - add warnings for short boundaries on right/bot
     n, m = size(grid)
-    if n == right || m == bot
-        error("Grid too small, add a right/bot edge")
-    end
+    (n == right) || (m == bot) && error("Grid too small, add a right/bot edge")
 
     left = findfirst(any.(>(0), eachcol(grid)))
     top = findfirst(any.(>(0), eachrow(grid)))
@@ -30,7 +30,7 @@ function shrink_grid(grid)
 end
 
 # TODO - Matrix{Bool} hack :(
-function RandomEnsemble(xs::Matrix{T}, f) where {T}
+function RandomEnsemble(xs::Matrix{T}, f::F) where {T,F}
     if T == Bool
         TT = Float64
     else
@@ -43,43 +43,63 @@ function RandomEnsemble(xs::Matrix{T}, f) where {T}
     w = zeros(TT, n, m)
     tw = zeros(TT, n, m)
     s = zeros(TT, n, m)
-    RandomEnsemble{TT}(f, b, #=vb,=# w, tw, s)
+    RandomEnsemble{TT,F}(f, b, w, tw, s) #=vb,=#
 end
 
 # TODO 1-liner?
-@inline function walk!(re::RandomEnsemble{T}, i, j) where {T}
+@inline function walk!(i, j)
     n = rand(((0, 1), (0, -1), (1, 0), (-1, 0)))
-    return i + n[1], j + n[2]
+    @inbounds i += n[1]
+    @inbounds j += n[2]
+    return i, j
 end
-valid(re::RandomEnsemble{T}, i, j) where {T} = re.bitmat[i, j]
+valid(re::RandomEnsemble{T,F}, i, j) where {T,F} = re.bitmat[i, j]
 
-function trajectory!(re::RandomEnsemble{T}, i, j) where {T}
+function trajectory!(re::RandomEnsemble{T,F}, i, j) where {T,F}
+    n, m = size(re.sol)
     if !valid(re, i, j)
-        return re.sol
+        return
     end
     #@assert all(==(0), re.temp_walkers)::Bool
-    re.temp_walkers .= zero(eltype(T))
+    myzero = zero(eltype(T))
+    #re.temp_walkers .= zero(eltype(T))
+    #@tullio re.temp_walkers[i, j] = myzero
+    #@turbo for j in 1:m
+    #    for i in 1:n
+    #        re.temp_walkers[i, j] = myzero
+    #    end
+    #end
 
     # TODO -> keep top/bot/left/right maximums and extract that window
     while valid(re, i, j)
-        i, j = walk!(re, i, j)
-        re.temp_walkers[i, j] += 1
+        i, j = walk!(i, j)
+        @inbounds re.temp_walkers[i, j] += 1
     end
-    scalar::T = re.f(i, j)
+    scalar = re.f(i, j)
 
     # update!
-    re.sol .+= re.temp_walkers .* scalar
-    re.walkers .+= re.temp_walkers
+    #re.sol[:,:] .+= re.temp_walkers .* scalar
+    # Idea - single pass?
+    #@tullio re.sol[i, j] += re.temp_walkers[i, j] * scalar
+    #re.walkers .+= re.temp_walkers
+    #@tullio re.walkers[i, j] += re.temp_walkers[i, j]
+    @turbo for j in 1:m
+        for i in 1:n
+            re.walkers[i, j] += re.temp_walkers[i, j]
+            re.sol[i, j] += re.temp_walkers[i, j] * scalar
+            re.temp_walkers[i, j] = myzero
+        end
+    end
 
     #@assert any(!=(0), re.temp_walkers)::Bool
     # Actually only need to clear before you use it, not twice!
-    re.temp_walkers .= zero(eltype(T))
+    #re.temp_walkers .= zero(eltype(T))
     #@assert all(==(0), re.temp_walkers)::Bool
-    return re.sol
+    return
 end
 
 
-function solve!(re::RandomEnsemble{T}, k::Int) where {T}
+function solve!(re::RandomEnsemble{T,F}, k::Int) where {T,F}
     n, m = size(re.bitmat)
     # TODO - only iterate over valid indexes
     while all(<(k), re.walkers)::Bool
@@ -87,8 +107,8 @@ function solve!(re::RandomEnsemble{T}, k::Int) where {T}
             for i in 1:n
                 trajectory!(re, i, j)
             end
+            all(>=(k), re.walkers)::Bool && break
         end
-        all(>=(k), re.walkers)::Bool && break
     end
     return re
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,81 +7,83 @@ using Random
 using HDF5
 
 #@testset "RandomWalkBVP" showtiming=true begin
-    @testset "Construction" showtiming=true begin 
-        mat = h5read("hdf5mat", "placa");
-        f(x, y) = 1 / (y + 1)
-        re = RandomEnsemble(mat, f)
-        @test all(==(0), re.temp_walkers)
-        @test all(==(0), re.sol)
-        @test all(==(0), re.walkers)
+@testset "Construction" showtiming = true begin
+    mat = h5read("hdf5mat", "placa")
+    f(x, y) = 1 / (y + 1)
+    re = RandomEnsemble(mat, f)
+    @test all(==(0), re.temp_walkers)
+    @test all(==(0), re.sol)
+    @test all(==(0), re.walkers)
+end
+@testset "Basic functions" showtiming = true begin
+
+    m = [0 0 0; 0 1 0; 0 0 0]
+    b = BitArray(first(m) .!= m)
+    f(x, y) = 1 / (y + 1)
+    w = zeros(3, 3)
+    tw = zeros(3, 3)
+    sol = zeros(3, 3)
+    vb = @view m[b]
+    re = RandomEnsemble{Float64,typeof(f)}(f, b, w, tw, sol) #=vb, =#
+    Random.seed!(42)
+    @test re.bitmat == [false false false; false true false; false false false]
+    @test sum(re.walkers) == 0
+    # Only clear before using!
+    @test sum(re.temp_walkers) == 0
+    @test sum(re.sol) == 0
+    @test sum(re.bitmat) == 1
+    trajectory!(re, 2, 2)
+    # 
+    @test sum(re.temp_walkers) == 0
+    @test sum(re.walkers) == 1
+    @test sum(re.sol) ≈ 0.5
+    @test sum(re.bitmat) == 1
+
+    for _ in 1:100
+        @test walk!(1, 1) ∈ ((2, 1), (0, 1), (1, 2), (1, 0))
     end
-    @testset "Basic functions"  showtiming=true begin
-
-        m = [0 0 0; 0 1 0; 0 0 0]
-        b = BitArray(first(m) .!= m)
-        f(x, y) = 1 / (y + 1)
-        w = zeros(3, 3)
-        tw = zeros(3, 3)
-        sol = zeros(3, 3)
-        vb = @view m[b]
-        re = RandomEnsemble{Float64}(f, b, #=vb, =# w, tw, sol)
-        Random.seed!(42)
-        @test re.bitmat == [false false false; false true false; false false false]
-        @test sum(re.walkers) == 0
-        @test sum(re.temp_walkers) == 0
-        @test sum(re.sol) == 0
-        @test sum(re.bitmat) == 1
-        trajectory!(re, 2, 2)
-        @test sum(re.temp_walkers) == 0
-        @test sum(re.walkers) == 1
-        @test sum(re.sol) ≈ 0.5
-        @test sum(re.bitmat) == 1
-
-        for _ in 1:100
-            re = RandomEnsemble{Float64}(f, b, #=vb,=# w, tw, sol)
-            @test walk!(re, 1, 1) ∈ ((2, 1), (0, 1), (1, 2), (1, 0))
-        end
-        # TODO - Fix ... ?
-        #@test 0 == @allocated walk!(re, 1, 1)
-    end
+    # TODO - Fix ... ?
+    #@test @allocations(walk!(1,1)) == 0
+end
 
 
-    @testset "Shrink grid" showtiming=true begin
-        mat = h5read("hdf5mat", "placa");
-        shs = shrink_grid(mat)
-        @test size(shs) == (335, 389)
-        @test 2 == findfirst(any.(>(0), eachrow(shs)))
-        @test 334 == findlast(any.(>(0), eachrow(shs)))
-        @test 2 == findfirst(any.(>(0), eachcol(shs)))
-        @test 388 == findlast(any.(>(0), eachcol(shs)))
-        # TODO idempotencia shs == eng
-        @test shs == shrink_grid(shs)
-    end
+@testset "Shrink grid" showtiming = true begin
+    mat = h5read("hdf5mat", "placa")
+    shs = shrink_grid(mat)
+    @test size(shs) == (335, 389)
+    @test 2 == findfirst(any.(>(0), eachrow(shs)))
+    @test 334 == findlast(any.(>(0), eachrow(shs)))
+    @test 2 == findfirst(any.(>(0), eachcol(shs)))
+    @test 388 == findlast(any.(>(0), eachcol(shs)))
+    # TODO idempotencia shs == eng
+    @test shs == shrink_grid(shs)
+end
 
-    @testset "solve Laplace 3x3 single step"  showtiming=true begin
-        m = [0 0 0; 0 1 0; 0 0 0]
-        b = BitArray(first(m) .!= m)
-        f(x, y) = 1 / (y + 1)
-        w = zeros(3, 3)
-        tw = zeros(3, 3)
-        sol = zeros(3, 3)
-        vb = @view m[b]
-        re = RandomEnsemble{Float64}(f, b, #=vb,=# w, tw, sol)
-        Random.seed!(42)
+@testset "solve Laplace 3x3 single step" showtiming = true begin
+    m = [0 0 0; 0 1 0; 0 0 0]
+    b = BitArray(first(m) .!= m)
+    f(x, y) = 1 / (y + 1)
+    w = zeros(3, 3)
+    tw = zeros(3, 3)
+    sol = zeros(3, 3)
+    vb = @view m[b]
+    re = RandomEnsemble{Float64,typeof(f)}(f, b, w, tw, sol) #=vb,=#
+    Random.seed!(42)
 
-        solve!(re, 1)
-        @test sum(re.sol) ≈ 0.5
-        @test sum(re.walkers) == 1
-        @test sum(re.temp_walkers) == 0
-    end
+    @test sum(re.temp_walkers) == 0
+    solve!(re, 1)
+    @test sum(re.sol) ≈ 0.5
+    @test sum(re.walkers) == 1
+    @test sum(re.temp_walkers) == 0
+end
 
-    @testset "Solve Laplace 334x389" showtiming=true begin
-        using HDF5
-        using Random
-        mat = h5read("hdf5mat", "placa")
-        f(x, y) = 1 / (y + 1)
-        re = RandomEnsemble(mat, f)
-        Random.seed!(42)
-        solve!(re, 0)
-    end
+@testset "Solve Laplace 334x389" showtiming = true begin
+    using HDF5
+    using Random
+    mat = h5read("hdf5mat", "placa")
+    f(x, y) = 1 / (y + 1)
+    re = RandomEnsemble(mat, f)
+    Random.seed!(42)
+    solve!(re, 1)
+end
 #end
